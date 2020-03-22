1b135827097aa92f15686f62d186ddb404de34de
Jennifer Yoo <jennifer.yoo7@gmail.com>
2020-03-22 02:28:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def line(deli)
+  if deli == []
+    puts "The line is currently empty."
+  else
+    display = "The line is currently:"
+    deli.each_with_index {|name, index| display += " #{index+1}. #{name}" }
+      puts "#{display}"
+  end
+end
+
+def take_a_number(katz_deli, last_person)
+  number = katz_deli.count(last_person)
+    puts "Welcome, #{last_person}. You are number #{number} in line."
+end
